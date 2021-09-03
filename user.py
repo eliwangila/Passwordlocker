19c6deacd3a7f25f94445ecad1a1e11d5a845f46
eliwangila <ekirapaeli254@gmail.com>
2021-09-03 17:04:17 +0300

    add methods to find credentails and user in the list

--- user.py
+++ user.py
@@ -18,3 +18,29 @@ class User:
 
         self.user_name = user_name
         self.user_password = user_password
+
+    def save_user(self):
+        """
+        saving a user to the user list
+        """
+        User.user_list.append(self)
+
+        #finding a user's credentials
+    @classmethod
+    def find_credentials (cls, name ):
+        """
+        checks correct importation
+        
+        Args:
+            name: credentials name
+        
+        Returns:
+            Boolean : True / False depending on if the credential exists or not
+        """
+        
+        #to search in the user list
+        for credentials in Credentials.credentials_list:
+            if credentials.credentials_name == name:
+                return True
+        
+        return False
